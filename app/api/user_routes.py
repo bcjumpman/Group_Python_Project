2d30efa8b1410a8fec6d678f05c52dced3e36747
Austin <hnitsua13@gmail.com>
2024-04-15 14:46:03 -0700

    Forms added, user routes started

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import User
 
@@ -12,7 +12,7 @@ def users():
     Query for all users and returns them in a list of user dictionaries
     """
     users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
+    return jsonify({'users': [user.to_dict() for user in users]})
 
 
 @user_routes.route('/<int:id>')
@@ -22,4 +22,49 @@ def user(id):
     Query for a user by id and returns that user in a dictionary
     """
     user = User.query.get(id)
-    return user.to_dict()
+
+    if not user:
+        return {"errors": "User not found"}, 404
+
+    return jsonify(user.to_dict())
+
+
+# # user_songs
+# @user_routes.route('/<int:id>/songs')
+# def song(id):
+#     user = User.query.get(id)
+
+#     if user:
+#         songs = user.songs.all()
+
+#     return songs
+
+
+# create
+# @user_routes.route('/', methods=['POST'])
+# def create_user():
+#     # Get form data
+#     data = request.get_json()
+
+#     user = dict()
+
+#     # update song with form data
+#     user['first_name'] = data['first_name']
+#     user['last_name'] = data['last_name']
+#     user['artist_name'] = data['artist_name']
+#     user['artist_country'] = data['artist_country']
+#     user['artist_bio'] = data['artist_bio']
+
+#     return jsonify({"user": user})
+
+
+# update
+@user_routes.route('/<int:id>', methods=['PUT'])
+def update_user(id):
+    return
+
+
+# delete
+@user_routes.route('/<int:id>', methods=['DELETE'])
+def remove_user(id):
+    return
---
+++ app/forms/user_form.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class UserForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(max=255)])
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    is_artist = BooleanField("Are you an artist?", validators=[DataRequired()])
+    artist_name = StringField("Stage Name")
+    artist_country = StringField("Country")
+    artist_bio = TextAreaField("Biography")
+    submit = SubmitField("Submit")
