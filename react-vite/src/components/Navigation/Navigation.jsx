819fc65311bb243336daf95d428a98e75de45f01
Austin <hnitsua13@gmail.com>
2024-04-16 11:21:15 -0700

    On signup modal

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -35,7 +35,7 @@ function Navigation({ isLoaded }) {
   return (
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">Music Haze</NavLink>
       </li>
 
       <li>
--- react-vite/src/components/SignupFormModal/SignupFormModal.jsx
+++ react-vite/src/components/SignupFormModal/SignupFormModal.jsx
@@ -6,10 +6,16 @@ import "./SignupForm.css";
 
 function SignupFormModal() {
   const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isArtist, setIsArtist] = useState("")
+  const [artistName, setArtistName] = useState("")
+  const [artistCountry, setArtistCountry] = useState("")
+  const [artistBio, setArtistBio] = useState("")
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
 
@@ -25,9 +31,15 @@ function SignupFormModal() {
 
     const serverResponse = await dispatch(
       thunkSignup({
+        firstName,
+        lastName,
         email,
         username,
         password,
+        isArtist,
+        artistName,
+        artistCountry,
+        artistBio
       })
     );
 
@@ -38,21 +50,57 @@ function SignupFormModal() {
     }
   };
 
+  const invalidInfo = () => {
+    return (
+      !email ||
+      !username ||
+      !firstName ||
+      !lastName ||
+      !password ||
+      !confirmPassword ||
+      !isArtist ||
+      !artistName ||
+      !artistCountry
+    );
+  };
+
+  const disabledButton = () => {
+    return (firstName === "" || lastName === "" || email === "" || username === "" || confirmPassword === "" || isArtist === "" || artistName === "" || artistCountry === "")
+  }
+
   return (
-    <>
+    <div className="signupModalWrapper">
       <h1>Sign Up</h1>
       {errors.server && <p>{errors.server}</p>}
-      <form onSubmit={handleSubmit}>
+      <form className="signupForm" onSubmit={handleSubmit}>
+        <label>
+          First Name
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Last Name
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </label>
         <label>
           Email
           <input
-            type="text"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </label>
-        {errors.email && <p>{errors.email}</p>}
+        {errors.email && <p className="errorMessage">{errors.email}</p>}
         <label>
           Username
           <input
@@ -62,7 +110,7 @@ function SignupFormModal() {
             required
           />
         </label>
-        {errors.username && <p>{errors.username}</p>}
+        {errors.username && <p className="errorMessage">{errors.username}</p>}
         <label>
           Password
           <input
@@ -72,7 +120,7 @@ function SignupFormModal() {
             required
           />
         </label>
-        {errors.password && <p>{errors.password}</p>}
+        {errors.password && <p className="errorMessage">{errors.password}</p>}
         <label>
           Confirm Password
           <input
@@ -82,10 +130,60 @@ function SignupFormModal() {
             required
           />
         </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit">Sign Up</button>
+        {errors.confirmPassword && <p className="errorMessage">{errors.confirmPassword}</p>}
+        <label>
+          Are You An Artist?
+          <input
+            type="radio"
+            value="true"
+            checked={isArtist === true}
+            onChange={() => setIsArtist(true)}
+            required
+          /> Yes
+          <input
+            type="radio"
+            value="false"
+            checked={isArtist === false}
+            onChange={() => setIsArtist(false)}
+            required
+          /> No
+        </label>
+        {errors.isArtist && <p className="errorMessage">{errors.isArtist}</p>}
+        <label>
+          Artist Name
+          <input
+            type="text"
+            value={artistName}
+            onChange={(e) => setArtistName(e.target.value)}
+            required
+          />
+        </label>
+        {errors.artistName && <p className="errorMessage">{errors.artistName}</p>}
+        <label>
+          Country
+          <input
+            type="text"
+            value={artistCountry}
+            onChange={(e) => setArtistCountry(e.target.value)}
+            required
+          />
+        </label>
+        {errors.artistCountry && <p className="errorMessage">{errors.artistCountry}</p>}
+        <label>
+          Biography
+          <input
+            type="textarea"
+            value={artistBio}
+            onChange={(e) => setArtistBio(e.target.value)}
+            required
+          />
+        </label>
+        {errors.artistBio && <p className="errorMessage">{errors.artistBio}</p>}
+        {disabledButton() ?
+          <button className="disabledSignupButton" disabled={true} type="submit">Sign Up</button>
+          : <button className="signupModalButton" disabled={invalidInfo()} type="submit">Sign Up</button>}
       </form>
-    </>
+    </div>
   );
 }
 
