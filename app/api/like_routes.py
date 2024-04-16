1df39f38e0738789cd084c4a39bee3a83d4540fc
Brandon <myboyknuckles@gmail.com>
2024-04-16 09:15:31 -0500

    Backend routes for comments, likes, and songs are all completed# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Date:      Tue Apr 16 09:15:31 2024 -0500## On branch kiante# Your branch is ahead of 'origin/kiante' by 1 commit.#   (use "git push" to publish your local commits)## Changes to be committed:#	modified:   app/api/comment_routes.py#	modified:   app/api/like_routes.py#	modified:   app/api/song_routes.py#

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,25 +1,32 @@
-from flask import Blueprint, jsonify
-from app.models import Like
+from flask import Blueprint, jsonify, request
+from app.models import db, Like
 
 
 like_routes = Blueprint('likes', __name__)
 
-# @like_routes.route('/')
-# def getAllLikes():
-#   likes = Like.query.all()
-#   return {'likes': [like.to_dict() for like in likes]}
+@like_routes.route('/')
+def getAllLikes():
+  likes = Like.query.all()
+  return {'likes': [like.to_dict() for like in likes]}
 
-# @like_routes.route('<int:id>')
-# def getLikeById(id):
-#   like = Like.query.all()
-#   return like.to_dict()
+@like_routes.route('/', methods=["POST"])
+def addNewLike():
+  data = request.get_json()
 
-# @like_routes.route('<int:id>', methods=["POST"])
-# def getLikeById(id):
-#   like = Like.query.all()
-#   return like.to_dict()
+  like = Like(
+    user_id = data['user_id'],
+    song_id = data['song_id']
+  )
 
-# @like_routes.route('<int:id>/delete')
-# def deleteLikeById(id):
-#   like = Like.query.all()
-#   return like.to_dict()
+  db.session.add(like)
+  db.session.commit()
+
+  return {"new_like": like.to_dict()}
+
+@like_routes.route('<int:id>', methods=["DELETE"])
+def deleteLikeById(id):
+  #CHANGE to find like on backend using song_id and user_id
+  like = Like.query.get(id)
+  db.session.delete(like)
+  db.session.commit()
+  return jsonify(f"Like was removed from song {like.song_id}")
--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, jsonify, request
 from app.models import db, Song, Like, Comment
-
+import wave
 
 song_routes = Blueprint('songs', __name__)
+
+## Read the music file to retrieve duration
+# def get_duration_wave(file_path):
+#    with wave.open(file_path, 'r') as audio_file:
+#       frame_rate = audio_file.getframerate()
+#       n_frames = audio_file.getnframes()
+#       duration = n_frames / float(frame_rate)
+#       return duration
 
 # Get all songs with like
 @song_routes.route('/')
@@ -16,41 +24,85 @@ def getAllSongs():
     per_song['likes'] = len(song.likes)
     all_songs.append(per_song)
 
+  # if request.args:
+  #   filter_list = []
+  #   filtered = []
+
+  #   for arg in request.args:
+  #     filter_list.append((arg, request.args[arg]))
+
+  #   def dynamic_filter(variable):
+  #     if request.args[arg] in all_songs[arg]:
+  #       return True
+  #     else:
+  #       return False
+
+  #   filtered = filter(dynamic_filter, all_songs)
+  #   print(filtered)
+
+  #   return {'message': "this is filtered"}
+
+
   return jsonify({'songs': all_songs})
 
+# Create new song
+@song_routes.route('/', methods=["POST"])
+def createNewSong():
+  data = request.get_json()
+
+  new_song = Song(
+    name = data['name'],
+    user_id = data['user_id'],
+    song_url = data['song_url'],
+    cover_art = data['cover_art'],
+    genre = data['genre'],
+    is_private = data['is_private'],
+    # duration = get_duration_wave(data['song_url'])
+    duration = 888,
+    plays = 0
+  )
+
+  db.session.add(new_song)
+  db.session.commit()
+
+  return jsonify({"song": new_song.to_dict()})
+
 # Get song by id
-@song_routes.route('/<int:id>', methods=["GET"])
+@song_routes.route('<int:id>', methods=["GET"])
 def getSongById(id):
   song = db.session.query(Song).join(Like, Song.id == Like.song_id).join(Comment, Song.id == Comment.song_id).filter(Song.id == id).one()
+  if not song:
+    return jsonify({"message": f'No song was located with the id of {id}'})
+
   query_song = song.to_dict()
   query_song['likes'] = len(song.likes)
   query_song['comments'] = [comment.to_dict() for comment in song.comments]
 
   return jsonify({"song": query_song})
 
-@song_routes.route('/<int:id>', methods=["POST"])
+@song_routes.route('<int:id>', methods=["PUT"])
 def updateSongById(id):
-  # Get song
-  song = Song.query.get(id)
-
-  # Get form data
   data = request.get_json()
+  song = Song.query.get(id)
+  if not song:
+    return jsonify({"message": f'No song was located with the id of {id}'})
 
-  # update song with form data
   song.name = data['name']
   song.song_url = data['song_url']
   song.cover_art = data['cover_art']
   song.genre = data['genre']
   song.is_private = data['is_private']
 
-  # commit to db
   db.session.commit()
 
-  return song.to_dict()
+  return jsonify({"song": song})
 
-# @song_routes.route('/<int:id>', methods=["PUT"])
-
-# @song_routes.route('/<int:id>', methods=["DELETE"])
-# def deleteSongById(id):
-#   song = Song.query.all()
-#   return song.to_dict()
+# Delete song by id
+@song_routes.route('<int:id>', methods=["DELETE"])
+def deleteSongById(id):
+  song = Song.query.get(id)
+  if not song:
+    return jsonify({"message": f'No song was located with the id of {id}'})
+  db.session.delete(song)
+  db.session.commit()
+  return jsonify({"message": f"The song {song.name} was deleted successfully"})
