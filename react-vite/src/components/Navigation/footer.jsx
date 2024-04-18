ab5e6d4eb6f480cada551970e5ba5f9880725c71
Brian Carmichael <bdonjr7@gmail.com>
2024-04-18 15:38:16 -0700

    small change

--- react-vite/src/components/Navigation/footer.jsx
+++ react-vite/src/components/Navigation/footer.jsx
@@ -1,7 +1,4 @@
 import SongPlayer from "./SongPlayer/SongPlayer";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useEffect } from "react";
-// import { thunkGetSongs } from "../../redux/song";
 
 export default function Footer() {
   return <SongPlayer />;
