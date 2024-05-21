2dd71a25f127522924345f7b1ddfc1cf164e6b99
Brandon <myboyknuckles@gmail.com>
2024-05-21 09:11:37 -0500

    Fully styled the login and signup form

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import "./LoginForm.css";
+import "./LoginFormModal.css";
 
 function LoginFormModal() {
   const dispatch = useDispatch();
@@ -11,6 +11,7 @@ function LoginFormModal() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
+  const [btnDisabled, setBtnDisabled] = useState(true)
   const { closeModal } = useModal();
 
   const handleSubmit = async (e) => {
@@ -39,40 +40,44 @@ function LoginFormModal() {
     return false;
   }
 
+  useEffect(()=>{
+    if (!email || !password) {
+      setBtnDisabled(true)
+    } else {
+      setBtnDisabled(false)
+    }
+  }, [email, password])
+
   return (
     <div className="login-modal">
-      <h1 className="login-header">Log In</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label className="login-labels">
-          Email
-          <input
-            type="text"
-            className="login-inputs"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            className="login-inputs"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        {disabledButton() ?
-          <button className='login-button' type="submit" disabled={true}>
-            Log In
-          </button>
-          :
-          <button className='login-success' type="submit" >Log In</button>
-        }
-      </form>
+      <div className="overlay">
+        <h1 className="login-header">Log In</h1>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label className="login-labels">
+            Email
+            <input
+              type="text"
+              className="login-inputs"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          {errors.email && <p className="error">{errors.email}</p>}
+          <label className="login-labels">
+            Password
+            <input
+              type="password"
+              className="login-inputs"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          {errors.password && <p className="error">{errors.password}</p>}
+          <button className='login-button' type="submit" disabled={btnDisabled}>Log In</button>
+        </form>
+      </div>
     </div>
   );
 }
